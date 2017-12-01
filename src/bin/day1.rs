1f7007edade5831c8df5092b3e7fad145b5c4ce7
Steven Sheffey <stevensheffey4@gmail.com>
2017-12-01 11:16:16 -0600

    Added test cases

--- src/bin/day1.rs
+++ src/bin/day1.rs
@@ -1,5 +1,47 @@
 use std::io;
 
+/// Converts a string of numbers to a vector of digits
+fn string_to_digits(input: &str) -> Vec<u32> {
+    input
+        // Iterate over string's characters
+        .chars()
+        // Convert each char to a number
+        .map(|c| match c.to_digit(10) {
+            Some(digit) => digit,
+            // Just use 0 for errors
+            None => 0
+        })
+        .collect()
+}
+/// Solves part 1 of the puzzle
+fn solve_part1(captcha: &Vec<u32>) -> u64 {
+    // Store sum as u64 so we have more space 
+    let mut sum: u64 = 0;
+    // Iterate over the list and do both parts
+    for (index, digit) in captcha.iter().enumerate() {
+        // Check the next digit
+        if *digit == captcha[(index + 1) % captcha.len()] {
+            sum += *digit as u64;
+        }
+    }
+    // Return the sum
+    sum
+}
+
+/// Solves part 2 of the puzzle
+fn solve_part2(captcha: &Vec<u32>) -> u64 {
+    // Store sum as u64 so we have more space 
+    let mut sum: u64 = 0;
+    // Iterate over the list and do both parts
+    for (index, digit) in captcha.iter().enumerate() {
+        // Check the next digit
+        if *digit == captcha[(index + (captcha.len() / 2)) % captcha.len()] {
+            sum += *digit as u64;
+        }
+    }
+    // Return the sum
+    sum
+}
 fn main() {
     // Read a line from stdin
     let mut captcha = String::new();
@@ -8,26 +50,53 @@ fn main() {
         Err(error) => panic!("Failed to read a line from stdin. Error: {}", error),
     }
     // Convert string to a vector of digits
-    let captcha: Vec<u32> = captcha.trim().chars().map(|c| match c.to_digit(10) {
-        Some(digit) => digit,
-        None => 0
-    }).collect();
+    let captcha: Vec<u32> = string_to_digits(&captcha.trim());
     // Solve the first part
     // Won't be caught off-guard with a multiple-gigabyte file ;)
-    let mut part1_sum: u64 = 0;
-    let mut part2_sum: u64 = 0;
-    // Iterate over the list and do both parts
-    for (index, digit) in captcha.iter().enumerate() {
-        // Check the next digit
-        if *digit == captcha[(index + 1) % captcha.len()] {
-            part1_sum += *digit as u64;
-        }
-        // Check the digit halfway across the list
-        if *digit == captcha[(index + (captcha.len() / 2)) % captcha.len()] {
-            part2_sum += *digit as u64;
-        }
-    }
-    println!("Part 1: {}", part1_sum);
-    println!("Part 2: {}", part2_sum);
+    let part1_answer: u64 = solve_part1(&captcha);
+    let part2_answer: u64 = solve_part2(&captcha);
+    println!("Part 1: {}", part1_answer);
+    println!("Part 2: {}", part2_answer);
 }
-//TODO: build tests
+
+
+#[cfg(test)]
+mod day1_tests {
+    // Import needed functions
+    use string_to_digits;
+    use solve_part1;
+    use solve_part2;
+
+    #[test]
+    /// Test the digit converter
+    fn test_string_to_digits() {
+        // Check normal case
+        assert_eq!(string_to_digits("123456789"), vec![1,2,3,4,5,6,7,8,9]);
+        // Check empty case
+        assert_eq!(string_to_digits(""), vec![]);
+        // Check invalid characters
+        assert_eq!(string_to_digits("aaa"), vec![0,0,0]);
+        // Check invalid in mixture
+        assert_eq!(string_to_digits("123abc456"), vec![1,2,3,0,0,0,4,5,6]);
+    }
+    #[test]
+    /// Test solution for part 1
+    fn test_part1() {
+        // Examples from the problem
+        assert_eq!(solve_part1(&string_to_digits("1122")), 3);
+        assert_eq!(solve_part1(&string_to_digits("1111")), 4);
+        assert_eq!(solve_part1(&string_to_digits("1234")), 0);
+        assert_eq!(solve_part1(&string_to_digits("91212129")), 9);
+    }
+
+    #[test]
+    /// Test solution for part 2
+    fn test_part2() {
+        // Examples from the problem
+        assert_eq!(solve_part2(&string_to_digits("1212")), 6);
+        assert_eq!(solve_part2(&string_to_digits("1221")), 0);
+        assert_eq!(solve_part2(&string_to_digits("123425")), 4);
+        assert_eq!(solve_part2(&string_to_digits("123123")), 12);
+        assert_eq!(solve_part2(&string_to_digits("12131415")), 4);
+    }
+}
