7f43073aa14ce1e18eff79efa92fb30fe9af6435
Steven Sheffey <stevensheffey4@gmail.com>
2017-12-04 14:22:17 -0600

    Better comments

--- src/bin/day4.rs
+++ src/bin/day4.rs
@@ -15,14 +15,17 @@ fn main() {
                 Ok(line) => line,
                 Err(error) => panic!("Failed to get line from stdin. Error: {}", error),
             };
-            // Trim the line and create a split iterator
+            // Trim newlines off the line and get it as a list of string slices
             let line: Vec<&str> = line.trim().split(" ").collect();
-            // Build a hashset and
+            // Build a hashset and test its size
+            // If hashset.len == line.len, all words in the line are unique
             HashSet::<&str>::from_iter(line.iter().cloned()).len() == line.len()
         })
+        // Reduce over the list to get the number
+        // of lines where all words are unique
         .fold(
             0,
-            |sum, is_identical| sum + if is_identical { 1 } else { 0 },
+            |sum, is_unique| sum + if is_unique { 1 } else { 0 },
         );
     println!("{:?}", result)
 }
