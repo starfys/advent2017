87e9ce37003c950c28eca167dc0c28976ce6132f
Steven Sheffey <stevensheffey4@gmail.com>
2017-12-04 16:48:19 -0600

    Moved solution to a function

--- src/bin/day4.rs
+++ src/bin/day4.rs
@@ -3,45 +3,55 @@ use std::io;
 use std::io::prelude::*;
 use std::iter::FromIterator;
 
-fn main() {
-    let stdin = io::stdin();
+fn solve_day4(lines: &Vec<String>) -> (usize, usize) {
     // Map over stdin's lines
-    let (part1_answer, part2_answer): (usize, usize) = stdin
-        .lock()
-        .lines()
+    lines
+        .iter()
         .map(|line| {
-            // Unwrap the line
-            let line: String = match line {
-                Ok(line) => line,
-                Err(error) => panic!("Failed to get line from stdin. Error: {}", error),
-            };
             // Trim newlines off the line and get it as a list of string slices
             let line: Vec<&str> = line.trim().split(" ").collect();
             // Build a hashset and test its size
             // If hashset.len == line.len, all words in the line are unique
             let num_unique: usize = HashSet::<&str>::from_iter(line.iter().cloned()).len();
-            let num_permutations_unique: usize = HashSet::<Vec<char>>::from_iter(line.iter().map(|word| {
+            let num_permutations_unique: usize =
+            HashSet::<Vec<char>>::from_iter(line.iter().map(|word| {
                 // Get the word's characters
                 let mut word_characters: Vec<char> = word.chars().collect();
                 // Sort the vec
                 word_characters.sort();
-                // Join the word characters
                 // Return the vector
+                // Only need to count unique vectors, so don't bother converting back to a string
                 word_characters
             })).len();
             // Return whether all are unique for both cases
-            (num_unique == line.len(), num_permutations_unique == line.len())
-        })
-        // Reduce over the list to get the number
-        // of lines where all words are unique
-        .fold(
-            (0, 0),
-            |(part1_sum, part2_sum), (all_unique, all_permutations_unique)| 
             (
-                part1_sum + if all_unique { 1 } else { 0 },
-                part2_sum + if all_permutations_unique {1} else {0}
+                num_unique == line.len(),
+                num_permutations_unique == line.len(),
             )
-        );
+        })
+        .fold((0, 0), |(num_unique_rows, num_permuted_unique_rows),
+         (is_unique, is_permuted_unique)| {
+            (
+                num_unique_rows + is_unique as usize,
+                num_permuted_unique_rows + is_permuted_unique as usize,
+            )
+        })
+}
+
+fn main() {
+    // Get a handle on stdin
+    let stdin = io::stdin();
+    // Get all lines from stdin
+    let input_lines: Vec<String> = stdin
+        .lock()
+        .lines()
+        .map(|line| match line {
+            Ok(line) => line,
+            Err(error) => panic!("Failed to read line. Error: {}.", error),
+        })
+        .collect();
+    // Solve the problem
+    let (part1_answer, part2_answer): (usize, usize) = solve_day4(&input_lines);
     println!("Part 1: {}", part1_answer);
     println!("Part 2: {}", part2_answer);
 }
