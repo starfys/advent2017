f321df1416505a281cd41a19119e1273badb6127
Steven Sheffey <stevensheffey4@gmail.com>
2017-12-04 14:36:27 -0600

    Added day 4 part 2

--- src/bin/day4.rs
+++ src/bin/day4.rs
@@ -6,7 +6,7 @@ use std::iter::FromIterator;
 fn main() {
     let stdin = io::stdin();
     // Map over stdin's lines
-    let result: usize = stdin
+    let (part1_answer, part2_answer): (usize, usize) = stdin
         .lock()
         .lines()
         .map(|line| {
@@ -19,13 +19,29 @@ fn main() {
             let line: Vec<&str> = line.trim().split(" ").collect();
             // Build a hashset and test its size
             // If hashset.len == line.len, all words in the line are unique
-            HashSet::<&str>::from_iter(line.iter().cloned()).len() == line.len()
+            let num_unique: usize = HashSet::<&str>::from_iter(line.iter().cloned()).len();
+            let num_permutations_unique: usize = HashSet::<Vec<char>>::from_iter(line.iter().map(|word| {
+                // Get the word's characters
+                let mut word_characters: Vec<char> = word.chars().collect();
+                // Sort the vec
+                word_characters.sort();
+                // Join the word characters
+                // Return the vector
+                word_characters
+            })).len();
+            // Return whether all are unique for both cases
+            (num_unique == line.len(), num_permutations_unique == line.len())
         })
         // Reduce over the list to get the number
         // of lines where all words are unique
         .fold(
-            0,
-            |sum, is_unique| sum + if is_unique { 1 } else { 0 },
+            (0, 0),
+            |(part1_sum, part2_sum), (all_unique, all_permutations_unique)| 
+            (
+                part1_sum + if all_unique { 1 } else { 0 },
+                part2_sum + if all_permutations_unique {1} else {0}
+            )
         );
-    println!("{:?}", result)
+    println!("Part 1: {}", part1_answer);
+    println!("Part 2: {}", part2_answer);
 }
